7ee4e30ca287373ef0266a2ddeaf4b8eebf174e0
Jihu Nam <jihuu2806@gmail.com>
2025-10-29 01:43:29 +0900

    chore: H2 기반 초기 백엔드 환경 세팅

---
+++ nutrigo-backend/src/main/java/com/nutrigo/nutrigo_backend/NutrigoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nutrigo.nutrigo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutrigoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutrigoBackendApplication.class, args);
+	}
+
+}
